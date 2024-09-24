abf3394a0ccbd1356f861d5e9cd55258f1739cd4
Cassio Santana <cassioo.roger@gmail.com>
2024-09-24 09:06:52 -0300

    build: add sidekiq gem
    Ref #1 #2 #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :ebooks do
     collection do
@@ -8,4 +10,6 @@ Rails.application.routes.draw do
     end
   end
   root "home#index"
+
+  mount Sidekiq::Web => "/sidekiq"
 end
